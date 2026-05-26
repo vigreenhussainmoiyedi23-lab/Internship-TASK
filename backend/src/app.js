70f4033fe882031275309007794e2909cd687534
vigreenhussainmoiyedi23-lab <vigreenhussainmoiyedi23@gmail.com>
2026-05-26 23:45:42 +0530

    ready for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,6 +20,12 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 
 app.use("/api/auth", require("./routes/auth.routes"))
 app.use("/api/messages", require("./routes/messages.routes.js"))
+// ✅ serve frontend
+const distPath = path.join(__dirname, "../public")
+app.use(express.static(distPath))
+app.get("*name", (req, res) => {
+    res.sendFile(path.join(distPath, "index.html"))
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`)
