c87340f58e50dccaa56f09629999cd03d67dd060
vigreenhussainmoiyedi23-lab <vigreenhussainmoiyedi23@gmail.com>
2026-05-26 23:46:55 +0530

    feat: Import path module for serving static files

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const cors = require("cors")
+const path = require("path")
 require("dotenv").config()
 const cookieParser = require("cookie-parser")
 
